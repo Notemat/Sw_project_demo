172c338b741dde3ef15abaff2db7080161b2127c
Никита Козлов <kozlov.nik@icloud.com>
2024-10-17 12:36:56 +0300

    fix map

--- sw_project/static/js/main.js
+++ sw_project/static/js/main.js
@@ -46,6 +46,31 @@ function openWhatsAppChat() {
   window.open('https://wa.me/79182000207', '_blank');
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+  let logo = document.querySelector('.logo_nav a'),
+      menu = document.querySelector('.bar_menu');
+
+  // Проверяем ширину экрана и только для мобильных включаем логику
+  function toggleMenu() {
+    if (window.innerWidth <= 768) {
+      menu.classList.toggle('open'); // Переключаем класс "open" для меню
+    }
+  }
+
+  // Добавляем обработчик события для клика по логотипу
+  logo.addEventListener('click', function(event) {
+    event.preventDefault(); // Предотвращаем стандартное поведение (переход по ссылке)
+    toggleMenu();
+  });
+
+  // Закрываем меню при клике за его пределами
+  document.addEventListener('click', function(e) {
+    if (!menu.contains(e.target) && !logo.contains(e.target) && menu.classList.contains('open')) {
+      menu.classList.remove('open');
+    }
+  });
+});
+
 
 AOS.init({
   offset: 200,
